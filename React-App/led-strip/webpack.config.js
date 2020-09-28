39f947a13ab3d86f93a53e7f4385d8a6f612b009
Kyler Freas <kmf5285@rit.edu>
2020-09-28 01:53:20 -0400

    enabled class properties

--- React-App/led-strip/webpack.config.js
+++ React-App/led-strip/webpack.config.js
@@ -23,8 +23,19 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader", 
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/react',
+              {
+                'plugins': [
+                  '@babel/plugin-proposal-class-properties'
+                ]
+              }
+            ]
+          }
+        }, 
       },
       
       {
